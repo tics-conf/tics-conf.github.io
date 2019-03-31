6e71202a7217db4d1d9fb6309617f16bbb9083bc
Marios Isaakidis <prometheas@autistici.org>
2019-03-31 19:59:27 +0100

    Remove baseURL from config.toml

--- publish_to_ghpages.sh
+++ publish_to_ghpages.sh
@@ -19,8 +19,8 @@ rm -rf .git/worktrees/public/
 echo "Checking out gh-pages branch into public"
 git worktree add -B gh-pages public origin/gh-pages
 
-echo "Initializing theme submodules"
-git submodule add https://github.com/redraw/hugo-mondrian-theme.git themes/mondrian
+echo "Initializing submodules"
+git submodule update --init --recursive
 
 echo "Removing existing files"
 rm -rf public/*
